3d01b437b7880ea425291460e5bf04f1a2c78d7c
SnO2WMaN <me@sno2wman.net>
2022-07-20 19:15:11 +0900

    setup env

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    isuconix.url = "github:SnO2WMaN/ISUCONix";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell,
+    isuconix,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+            isuconix.overlays.default
+          ];
+        };
+      in rec {
+        devShell = pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+          ];
+        };
+      }
+    );
+}
